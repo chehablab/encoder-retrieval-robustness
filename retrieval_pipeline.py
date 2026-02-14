4df79b24e370d51401756d4828480d2d38256805
GauravPatil8 <patilgauravpradeep@gmail.com>
2026-02-14 13:30:32 +0530

    add image transfomation in the pipeline

--- retrieval_pipeline.py
+++ retrieval_pipeline.py
@@ -8,21 +8,30 @@ from typing import List
 from datasets import get_dataset
 from encoders import get_encoder, get_features
 from metric import retrieval_mean_precision
+from transformations import get_transformation
 
 def exists(path):
     return os.path.exists(path)
 
-def _is_already_evaluated(checkpoint_file_path, encoder_name, dataset_name):
+def _is_already_evaluated(checkpoint_file_path, encoder_name, dataset_name, transformation_name):
     if exists(checkpoint_file_path):
         checkpoints = json.load(open(checkpoint_file_path))
         for checkpoint in checkpoints:
-            if checkpoint['encoder'] == encoder_name and checkpoint['dataset'] == dataset_name:
+            if checkpoint['encoder'] == encoder_name and checkpoint['dataset'] == dataset_name and checkpoint['transformation'] == transformation_name:
                 return True
     return False
+
+def _apply_transform(image, transformation):
+    img = np.asarray(image)
+    img = img / 255.0
+    augmented_img = (np.array(transformation([img])) * 255).astype(np.uint8)
+    return augmented_img
 
 def evaluate_retrieval(encoder_name: str, 
                        dataset_name: str, 
                        target_dim: int,
+                       transformation,
+                       transformation_name: str = None,
                        k_list: List[int] = [9],
                        device: str = "cuda",
                        checkpoint_folder: str = "./checkpoints", 
@@ -33,21 +42,34 @@ def evaluate_retrieval(encoder_name: str,
     if not exists(checkpoint_folder): os.mkdir(checkpoint_folder)
         
     checkpoint_file = os.path.join(checkpoint_folder, checkpoint_name+".json")
-    if _is_already_evaluated(checkpoint_file, encoder_name, dataset_name):
+    if _is_already_evaluated(checkpoint_file, encoder_name, dataset_name, transformation_name):
         print(f"{encoder_name} already evaluated on {dataset_name}. Skipping evaluation")
         return
 
     encoder, img_processor = get_encoder(encoder_name, device=device)
-    dataset = get_dataset(dataset_name, None, img_processor)
-        
-    if verbose: print(f"\nGetting image embeddings....")
+    dataset = get_dataset(dataset_name, 'train', None)
     num_samples = len(dataset)
+
+    if transformation:
+        if verbose: print(f"\n Augmenting images....")
+        all_images = []
+        all_labels = []
+
+        for image, label in tqdm(dataset):
+            aug_img = _apply_transform(image, transformation)
+            all_images.append(aug_img)
+            all_labels.append(label)
+        del dataset
+        dataset = zip(all_images, all_labels)
+    
+    if verbose: print(f"\nGetting image embeddings....")
+
     embeddings = torch.empty((num_samples, target_dim), device=device)
     labels = np.empty(num_samples, dtype=np.int32)
 
     encoder.eval()
     for i, (image, label) in enumerate(tqdm(dataset)):
-        image = image.to(device).unsqueeze(0)
+        image = img_processor(image, return_tensors="pt")["pixel_values"].to(device)
         emb = get_features(encoder, image, target_dim, device)
         embeddings[i] = emb
         labels[i] = label
@@ -66,6 +88,7 @@ def evaluate_retrieval(encoder_name: str,
     results = {
         'encoder': encoder_name,
         'dataset': dataset_name,
+        'transformation': transformation_name,
         'metrics': {
             'mean_precision' : mean_precision
         }
@@ -76,3 +99,20 @@ def evaluate_retrieval(encoder_name: str,
     checkpoint.append(results)
     
     json.dump(checkpoint, open(checkpoint_file, 'w'), ensure_ascii=True, indent=4)
+
+def _test_retreival_pipeline():
+    encoder_name = "microsoft/resnet-50"
+    dataset_name = "flowers102"
+    transformation_obj = {
+            "id": "jigsaw",
+            "nb_rows_min": 2,
+            "nb_rows_max": 5,
+            "nb_cols_min": 2,
+            "nb_cols_max": 5,
+            "max_steps_min": 2,
+            "max_steps_max": 5,
+            "active": True
+        }
+    transformation = get_transformation(transformation_obj)
+    transformation_name = "jigsaw"
+    evaluate_retrieval(encoder_name, dataset_name, 2048, transformation, transformation_name, [5])
